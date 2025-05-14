5768dc9e66f0cfac9c445daa0211dc2634ce7403
LaraLiberti <147068475+LaraLiberti@users.noreply.github.com>
2025-05-14 19:22:08 +0200

    Create 03.communities.overlap.R

---
+++ 03.communities.overlap.R
@@ -0,0 +1,27 @@
+# MULTIVARIATE ANALISYS of how species are related in TIME 
+install.packages("overlap")
+library(overlap)
+
+data(kerinci) # data from Kerinci-Seblat National Park in Sumatra, Indonesia
+kerinci
+head(kerinci)
+summary(kerinci)
+
+# Selecting the first species
+tiger <- kerinci[kerinci$Sps=="tiger",] # Sps=species
+
+# Selecting the tigers time
+timetig <- kerinci$Time*2*pi 
+# we multiply the the linear time to 2 pi greek to have a time in radiants
+
+densityPlot(timetig, rug=TRUE)
+
+# Selecting the macaque
+macaque <- kerinci[kerinci$Sps=="macaque",] # Sps=species
+
+# Selecting the macaque time
+timemac <- macaque$Time*2*pi 
+
+densityPlot(timemac, rug=TRUE)
+
+overlapPlot(timetig,timemac)
